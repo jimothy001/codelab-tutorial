a698c866b504ccc65f95f9d3d8f3c1a314b16495
James Yamada <james.yamada.01@gmail.com>
2025-10-13 20:32:22 -0400

    fix ImportProvidersFrom error

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,14 +11,12 @@ import { provideRouter } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom(
-      provideFirebaseApp(() => initializeApp(environment.firebase)),
-      provideFirestore(() => getFirestore()),
-      provideAuth(() => getAuth()),
-      provideFunctions(() => getFunctions()),
-      provideStorage(() => getStorage()),
-      provideMessaging(() => getMessaging())
-    ),
-    provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"codelab-tutorial-93aed","appId":"1:897744610925:web:6a2fc66872887b56170164","storageBucket":"codelab-tutorial-93aed.firebasestorage.app","apiKey":"AIzaSyAB53_MZ-OqQlPI88uHukxiY6SJWAwgecM","authDomain":"codelab-tutorial-93aed.firebaseapp.com","messagingSenderId":"897744610925","projectNumber":"897744610925","version":"2"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+    provideAuth(() => getAuth()),
+    provideFunctions(() => getFunctions()),
+    provideStorage(() => getStorage()),
+    provideMessaging(() => getMessaging())
   ],
 };
