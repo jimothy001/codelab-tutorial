c8f30799051a2d77bc72d86b1d7429f6c155a6b3
James Yamada <james.yamada.01@gmail.com>
2025-10-18 17:46:49 -0400

    add google auth

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,41 @@
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAB53_MZ-OqQlPI88uHukxiY6SJWAwgecM",
+//   authDomain: "codelab-tutorial-93aed.firebaseapp.com",
+//   projectId: "codelab-tutorial-93aed",
+//   storageBucket: "codelab-tutorial-93aed.firebasestorage.app",
+//   messagingSenderId: "897744610925",
+//   appId: "1:897744610925:web:6a2fc66872887b56170164"
+// };
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
 export const environment = {
   firebase: {
-    apiKey: 'API_KEY',
-    authDomain: 'PROJECT_ID.firebaseapp.com',
-    databaseURL: 'https://PROJECT_ID.firebaseio.com',
-    projectId: 'PROJECT_ID',
-    storageBucket: 'PROJECT_ID.appspot.com',
-    messagingSenderId: 'SENDER_ID',
-    appId: 'APP_ID',
-    measurementId: 'G-MEASUREMENT_ID',
+    apiKey: "AIzaSyAB53_MZ-OqQlPI88uHukxiY6SJWAwgecM",
+    authDomain: "codelab-tutorial-93aed.firebaseapp.com",
+    projectId: "codelab-tutorial-93aed",
+    storageBucket: "codelab-tutorial-93aed.firebasestorage.app",
+    messagingSenderId: "897744610925",
+    appId: "1:897744610925:web:6a2fc66872887b56170164"
   },
 };
+
+// export const environment = {
+//   firebase: {
+//     apiKey: 'API_KEY',
+//     authDomain: 'PROJECT_ID.firebaseapp.com',
+//     databaseURL: 'https://PROJECT_ID.firebaseio.com',
+//     projectId: 'PROJECT_ID',
+//     storageBucket: 'PROJECT_ID.appspot.com',
+//     messagingSenderId: 'SENDER_ID',
+//     appId: 'APP_ID',
+//     measurementId: 'G-MEASUREMENT_ID',
+//   },
+// };
---
+++ src/firebase-config.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAB53_MZ-OqQlPI88uHukxiY6SJWAwgecM",
+  authDomain: "codelab-tutorial-93aed.firebaseapp.com",
+  projectId: "codelab-tutorial-93aed",
+  storageBucket: "codelab-tutorial-93aed.firebasestorage.app",
+  messagingSenderId: "897744610925",
+  appId: "1:897744610925:web:6a2fc66872887b56170164"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
